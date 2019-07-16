ec24b347c2c8d445f61c8876e4fd6d8dfede911b
Daniel0205 <daniel.alejandro.diaz@correounivalle.edu.co>
2019-07-15 22:31:05 -0500

    pila inicializada

--- Sokoban /sokoban_prof.cpp
+++ Sokoban /sokoban_prof.cpp
@@ -7,32 +7,49 @@
 
 using namespace std;
 
+stack <string*> states;
 
 
-
-int main(int argc, char **argv){
+void leerArchivo(string fileName){
+		
+	ifstream tablero(fileName);
 	
-	ifstream tablero("nivel1.txt");
-	
-	vector<string> datos;
+	vector<string> file;
 	int endTable=0;
 	string texto;
+	
 	
 	while(!tablero.eof()){
 		tablero >> texto;
 		if(texto[0]!='W' && texto[0]!='0' && texto[0]!='X' && endTable==0){
-			endTable=datos.size();
+			endTable=file.size();
 			
 		}		
-		datos.push_back(texto);
+		file.push_back(texto);
 		
 	}
 	tablero.close();
-	
+	string* table = new string [endTable];
+
 	for (int i = 0; i <endTable; i++){
-		
-		cout << datos[i] << endl;
+		table[i]=file[i];			
+		//cout << table[i] << endl;
 	}
+
+	states.push(table);
+	
+
+}
+
+
+int main(int argc, char **argv){
+
+	leerArchivo("nivel1.txt");
+
+	for (int i = 0; i <states.top()->length(); i++){
+		cout << states.top()[i] << endl;
+	}	
+
 	
 	
 	
