7d4de869849a2f386e3341d0b7fd30fd6256802b
Daniel Diaz <Daniel_Diaz@iMac-de-MMV-Lab.local>
2019-07-24 17:36:44 -0500

    clase nodo creada

---
+++ ai-reactApp-master/modelHex/Node.js
@@ -0,0 +1,19 @@
+
+
+
+class Node{
+    constructor(father,position,type,profundidad){
+        this.father=father;
+        this.position=position;
+        this.type=type;
+        this.profundidad=profundidad;
+        this.utilidad=Infinity;
+        
+    }
+
+
+
+
+
+
+}
