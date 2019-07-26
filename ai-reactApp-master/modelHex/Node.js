b0e76ad081cf26dde14dd6842212e2b1c5681b57
STEBAN <steban.cadena@correounivalle.edu.co>
2019-07-26 03:37:00 -0500

    Funcion heuristica pero solo sirve para el jugador 1 pues no se tiene en cuenta de quien es la jugada

--- ai-reactApp-master/modelHex/Node.js
+++ ai-reactApp-master/modelHex/Node.js
@@ -1,5 +1,4 @@
-
-
+const Graph = require('node-dijkstra');
 
 export default class Node{
     constructor(father,state,type,depth){
@@ -30,13 +29,151 @@ export default class Node{
     }
 
     calculateHeuristic(){
-        this.utility=10;
+        if (this.type == "Max") this.utility=this.shortestPath(this.transpose(this.state)).length - this.shortestPath(this.state).length ;
+        else this.utility=this.shortestPath(this.state).length-this.shortestPath(this.transpose(this.state)).length;
+    }
+
+    shortestPath(board){
+        const graph = new Graph();
+        let size = board.length
+
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                
+                let key = i*size + j;
+                let neighbors = this.getNeighborhood(key,board);
+
+                if (j === 0) { 
+                    if (board[i][j]===0) {
+                        neighbors['L'] = 1; 
+                    }else if(board[i][j]===1){
+                        neighbors['L'] = 0; 
+                    }
+                }
+                if (j === size - 1) {
+                    if (board[i][j]===0) {
+                        neighbors['R'] = 1; 
+                    }else if(board[i][j]===1){
+                        neighbors['R'] = 0; 
+                    }
+                }
+
+                graph.addNode(key+'',neighbors); 
+            } 
+        }
+
+        let neighborsL = {};
+        let neighborsR = {};
+    
+        for (let i = 0; i < size; i++) {
+            if (board[i][0] === 0) {
+                neighborsL[(i * size) + ''] = 1;
+            }else if(board[i][0] === 1){
+                neighborsL[(i * size) + ''] = 0;
+            }
+            if (board[i][size - 1] === 0) {
+                neighborsR[(i * size + size - 1) + ''] = 1;
+            }else if(board[i][size - 1] === 1){
+                neighborsR[(i * size + size - 1) + ''] = 0;
+            }
+        }
+    
+        graph.addNode('L', neighborsL);
+        graph.addNode('R', neighborsR);
+
+        return graph.path('L','R');
+        
     }
 
 
+    getNeighborhood(key,board) {
+        let size = board.length;
+        let row = Math.floor(key / size);
+        let col = key % size;
+        let result = {};
+
+        if(this.insertNeighbor(row-1,col)){
+            let name = (row-1)*size + col;
+            if(board[row-1][col]=== 0){
+                result[name+'']=1;
+            }else if(board[row-1][col]=== 1){
+                result[name+'']=0;
+            }
+        }
+
+        if(this.insertNeighbor(row-1,col+1)){
+            let name = (row-1)*size + col;
+            if(board[row-1][col+1]=== 0){
+                result[name+'']=1;
+            }else if(board[row-1][col+1]=== 1){
+                result[name+'']=0;
+            }
+        }
+
+        if(this.insertNeighbor(row,col+1)){
+            let name = (row-1)*size + col;
+            if(board[row][col+1]=== 0){
+                result[name+'']=1;
+            }else if(board[row][col+1]=== 1){
+                result[name+'']=0;
+            }
+        }
+
+        if(this.insertNeighbor(row+1,col)){
+            let name = (row-1)*size + col;
+            if(board[row+1][col]=== 0){
+                result[name+'']=1;
+            }else if(board[row+1][col]=== 1){
+                result[name+'']=0;
+            }
+        }
+
+        if(this.insertNeighbor(row+1,col-1)){
+            let name = (row-1)*size + col;
+            if(board[row+1][col-1]=== 0){
+                result[name+'']=1;
+            }else if(board[row+1][col-1]=== 1){
+                result[name+'']=0;
+            }
+        }
+
+        if(this.insertNeighbor(row-1,col-1)){
+            let name = (row-1)*size + col;
+            if(board[row][col-1]=== 0){
+                result[name+'']=1;
+            }else if(board[row][col-1]=== 1){
+                result[name+'']=0;
+            }
+        }
+
+        return result;
+    }
 
 
+    insertNeighbor(arr,row,col){
+        if(row >= 0 && row < this.state.length && col >= 0 && col < this.state.length){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
+    transpose(board) {
+        let size = board.length;
+        let boardT = new Array(size);
+        for (let j = 0; j < size; j++) {
+            boardT[j] = new Array(size);
+            for (let i = 0; i < size; i++) {
+                boardT[j][i] = board[i][j];
+                if (boardT[j][i] === '1') {
+                    boardT[j][i] = '2';
+                } else if (boardT[j][i] === '2') {
+                    boardT[j][i] = '1';
+                }
+            }
+        }
+        return boardT;
+    }
 
 
 }
